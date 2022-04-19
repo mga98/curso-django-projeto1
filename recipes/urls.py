c138fe84c70d62a8fd64907748ecd5c597006122
Miguel Almeida <miguel.almeida1009@gmail.com>
2022-04-19 10:58:43 -0300

    Removed "contato" and "sobre".

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
-from .views import contato, home, sobre
+from .views import home
 
 urlpatterns = [
     path('', home, name='index'),
-    path('contato/', contato, name='index'),
-    path('sobre/', sobre, name='sobre')
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -6,11 +6,3 @@ def home(request):
     context = {}
     context['name'] = 'Miguel'
     return render(request, 'recipes/home.html', context)
-
- 
-def contato(request):
-    return render(request)
-
-
-def sobre(request):
-    return render(request)
