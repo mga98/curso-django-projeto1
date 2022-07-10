377c8d3d1635ec73bb024f95a39c0536480ce9da
Miguel Almeida <miguel.almeida1009@gmail.com>
2022-07-10 20:58:13 -0300

    Initial register view logic

--- authors/urls.py
+++ authors/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'authors'
+
 urlpatterns = [
-    path('register/', views.register_view, name='register_view')
+    path('register/', views.register_view, name='register_view'),
+    path('register/create/', views.register_create, name='create'),
 ]
