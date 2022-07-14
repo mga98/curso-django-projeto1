61574ef3cf76352fc60ac618da1977801d8b40b6
Miguel Almeida <miguel.almeida1009@gmail.com>
2022-07-14 18:04:17 -0300

    Register Form updated

--- authors/forms.py
+++ authors/forms.py
@@ -1,8 +1,34 @@
+from attr import attr, fields
 from django import forms
 from django.contrib.auth.models import User
 
 
+def add_attr(field, attr_name, attr_new_val):
+    existing = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing} {attr_new_val}'.strip()
+
+
+def add_placeholder(field, placeholder_val):
+    add_attr(field, 'placeholder', placeholder_val)
+
+
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Seu nome de usuário')
+        add_placeholder(self.fields['password'], 'Digite sua senha')
+        add_placeholder(self.fields['email'], 'Digite um e-mail válido')
+        add_placeholder(self.fields['first_name'], 'Digite seu primeiro nome')
+        add_placeholder(self.fields['last_name'], 'Digite seu ultimo nome')
+
+    password_confirm = forms.CharField(
+        label='Confirme sua senha',
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repita sua senha'
+        })
+    )
+
     class Meta:
         model = User
         fields = [
@@ -12,3 +38,13 @@ class RegisterForm(forms.ModelForm):
             'password',
             'email',
         ]
+
+        help_texts = {
+            'email': 'O e-mail precisa ser válido!'
+        }
+
+        error_messages = {
+            'username': {
+                'required': 'Você precisa preencher o campo de usuário!'
+            },
+        }
