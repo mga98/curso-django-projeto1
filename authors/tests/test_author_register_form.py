0721b6d2a13267e1bd55b85f747c6617de7071e7
Miguel Almeida <miguel.almeida1009@gmail.com>
2022-07-22 10:07:42 -0300

    Register tests upgrade

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -1,6 +1,8 @@
+from unittest import TestCase
+from django.urls import reverse
 from authors.forms import RegisterForm
-from django.test import TestCase
 from parameterized import parameterized
+from django.test import TestCase as DjangoTestCase
 
 
 class AuthorRegisterFormUnitTest(TestCase):
@@ -37,3 +39,27 @@ class AuthorRegisterFormUnitTest(TestCase):
         form = RegisterForm()
         current_label = form[field].field.label
         self.assertEqual(label, current_label)
+
+
+class AuthorRegisterFormIntegrationTest(DjangoTestCase):
+    def setUp(self, *args, **kwargs) -> None:
+        self.form_data = {
+            'username': 'user',
+            'first_name': 'first',
+            'last_name': 'last',
+            'email': 'email@email',
+            'password': 'Password12',
+            'password2': 'Password12',
+        }
+
+        return super().setUp(*args, **kwargs)
+
+    @parameterized.expand([
+        ('username', 'Você precisa preencher o campo de usuário!')
+    ])
+    def test_fields_cannot_be_empty(self, field, msg):
+        self.form_data[field] = ''
+        url = reverse('authors:create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+        self.assertIn(msg, response.content.decode('utf-8'))
+        
