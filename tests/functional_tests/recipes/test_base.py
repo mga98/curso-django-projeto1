4de4e25127c60cfec5817df561fda2d99ece849b
Miguel Almeida <miguel.almeida1009@gmail.com>
2022-10-25 09:42:14 -0300

    RecipeMixin created.

--- tests/functional_tests/recipes/test_base.py
+++ tests/functional_tests/recipes/test_base.py
@@ -2,8 +2,10 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from utils.browser import make_chrome_browser
 import time
 
+from recipes.tests.test_recipe_base import RecipeMixin
 
-class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
+
+class RecipeBaseFunctionalTest(StaticLiveServerTestCase, RecipeMixin):
     def setUp(self) -> None:
         self.browser = make_chrome_browser()
         return super().setUp()
--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,7 +1,9 @@
 from selenium.webdriver.common.by import By
 from .test_base import RecipeBaseFunctionalTest
+import pytest
 
 
+@pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
     def test_recipe_home_page_without_recipes(self):
         self.browser.get(self.live_server_url)
