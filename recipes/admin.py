0e02663a9fe0a45fa05cf6e040c7a264c8aba1c6
Miguel Almeida <miguel.almeida1009@gmail.com>
2022-11-03 15:13:22 -0300

    Recipes admin page configuration.

--- recipes/admin.py
+++ recipes/admin.py
@@ -9,4 +9,13 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('id', 'title', 'author', 'created_at', 'is_published', 'preparation_steps_is_html')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'id', 'description', 'slug', 'author__username')
+    list_filter = ('category', 'author', 'is_published', 'preparation_steps_is_html')
+    list_per_page = 10
+    list_editable = ('is_published', 'preparation_steps_is_html')
+    ordering = ('-id', '-created_at')
+    prepopulated_fields = {
+        'slug': ('title',)
+    }
