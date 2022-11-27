46e432e2febf3bc56b51ec70d5e68bcf544b468b
Miguel Almeida <miguel.almeida1009@gmail.com>
2022-11-27 10:04:13 -0300

    Configuration to postgres

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,7 @@
 from django.http import Http404
 from django.db.models import Q
 from django.core.paginator import Paginator
-from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404
 from django.views.generic import ListView, DetailView
 
 from .models import Category, Recipe
